eb1c1be0c4fcf7efe6e78b94af2ad72670f1a54a
mrlee <lkt900520@gmail.com>
2026-03-09 22:29:42 +0900

    feat: 스톡 버전 조회

---
+++ stock/src/main/java/com/mrlee/stock/contoller/StockController.java
@@ -0,0 +1,18 @@
+package com.mrlee.stock.contoller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/stocks")
+@RestController
+public class StockController {
+
+    @GetMapping("/version")
+    public ResponseEntity<String> checkAppVersion() {
+        String appVersion = "현재 종목 앱 1.0.0v";
+        return new ResponseEntity<>(appVersion, HttpStatus.OK);
+    }
+}
